79cd9575495ff31aa2161d1fffbe6fc71cb0cea4
bcart01v <38075349+bcart01v@users.noreply.github.com>
2024-05-07 14:14:20 -0500

    Refactored code to test

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,9 +1,13 @@
-export default function updateStudentGradeByCity(students, city, newGrades) {
-  return students
-    .filter(student => student.location === city)
-    .map(student => {
-      const gradeObj = newGrades.find(grade => grade.studentId === student.id);
-      const grade = gradeObj ? gradeObj.grade : 'N/A';
-      return { ...student, grade };
-    });
+export default function updateStudentGradeByCity(arr, city, newGrades) {
+  const filteredStudents = arr.filter((student) => student.location === city);
+  const gradedStudents = filteredStudents.map((student) => {
+    const studentGrade = newGrades.filter((grade) => grade.studentId === student.id);
+    if (studentGrade.length !== 0) {
+      Object.assign(student, { grade: studentGrade[0].grade });
+    } else {
+      Object.assign(student, { grade: 'N/A' });
+    }
+    return student;
+  });
+  return gradedStudents;
 }
