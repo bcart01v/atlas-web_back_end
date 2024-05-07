575bb12a17c2cc4f95d1a1745646541c98165bba
bcart01v <38075349+bcart01v@users.noreply.github.com>
2024-05-07 11:20:44 -0500

    Refactoring

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,13 +1,17 @@
 export default class Building {
   constructor(sqft) {
-    this._sqft = sqft;
-
-    if (this.evacuationWarningMessage === undefined) {
-      throw new Error("Class extending Building must override evacuationWarningMessage");
+    if (this.constructor !== Building) {
+      if (this.evacuationWarningMessage === undefined) {
+        throw new Error("Class extending Building must override evacuationWarningMessage");
+      }
     }
+    this._sqft = sqft;
   }
-
   get sqft() {
     return this._sqft;
+  }
+
+  set sqft(newSqft) {
+    this._sqft = sqft;
   }
 }
