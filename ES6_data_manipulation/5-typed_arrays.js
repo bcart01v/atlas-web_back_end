7c0f0d691537721beb8d390eb052defbcfb57efb
bcart01v <38075349+bcart01v@users.noreply.github.com>
2024-05-07 14:18:24 -0500

    Typed Arrays

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,16 @@
+export default function createInt8TypedArray(length, position, value) {
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+}
+
+let buffer = new ArrayBuffer(length);
+let int8View = new DataView(buffer);
+
+try {
+    int8View.setInt8(position, value);
+} catch (error) {
+    throw new Error('Position outside range');
+}
+
+return buffer;
+}
