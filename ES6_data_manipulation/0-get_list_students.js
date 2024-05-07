6fe90539b28d91bac1bda00729b0ce1bd1f69020
bcart01v <38075349+bcart01v@users.noreply.github.com>
2024-05-07 13:45:33 -0500

    Will this work?

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,7 @@
 export default function getListStudents() {
-  return [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columnbia' },
-    { id: 5, firstName: 'Serena', location: 'San Franscisco'},
-  ];
+  const firstentry = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
+  const secondentry = { id: 2, firstName: 'James', location: 'Columnbia' };
+  const thirdentry = { id: 5, firstName: 'Serena', location: 'San Franscisco'};
+  const ReturnArray = [firstentry, secondentry, thirdentry];
+  return ReturnArray;
 }
