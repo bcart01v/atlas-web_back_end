5c2ee133a88f0c76097f01d2f13821f481d4c1e7
bcart01v <38075349+bcart01v@users.noreply.github.com>
2024-05-07 10:58:28 -0500

    Lint corrections

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,9 +1,9 @@
-import Currency from "./3-currency";
+import Currency from './3-currency';
 // they aren't asking for TypeErrors, so I'm not doing it.
 export default class Pricing {
   constructor(amount, currency) {
     this._amount = amount;
-    this._currency = currency;
+    this._currency = Currency;
   }
   get amount() {
     return this._amount;
