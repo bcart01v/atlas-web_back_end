d469dac78f2625313767d8ddd7a7ec827eb6de6a
bcart01v <38075349+bcart01v@users.noreply.github.com>
2024-05-07 10:55:38 -0500

    Currency!

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,31 @@
+import Currency from "./3-currency";
+// they aren't asking for TypeErrors, so I'm not doing it.
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    this._amount = newAmount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    this._currency = newCurrency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this.currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
