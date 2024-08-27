a34a70f35bd263f9aa3734359b4cbd737f96537e
Benjamin Carter <38075349+bcart01v@users.noreply.github.com>
2024-08-27 15:58:02 -0500

    Floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import math
+"""
+This module takes a float and returns the floor of the float.
+"""
+
+
+def floor(n: float) -> int:
+    """
+    This function takes a float and returns the floor of the float.
+
+    Args:
+     n (float): a float argument.
+
+     Returns:
+     The Floor of the float.
+    """
+    newval = math.floor(n)
+    return newval
