11b6597d5f30e41cd5b1b518fbc1b33995c44c78
Benjamin Carter <38075349+bcart01v@users.noreply.github.com>
2024-08-27 16:13:37 -0500

    To String

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import math
+"""
+This module takes a float and returns a string representation
+Of the float.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    This function takes a float and returns the string representation.
+
+    Args:
+        n (float): a float that will be converted to string
+
+     Returns:
+        str(n): the float converted to a string.
+    """
+    return str(n)
