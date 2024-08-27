f30107971ddeafebe38cf5d61b14c4c243379c32
Benjamin Carter <38075349+bcart01v@users.noreply.github.com>
2024-08-27 14:28:13 -0500

    Added more documentation

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python3
 
-# The Following is a function that takes 2 floats, and adds them together.
-# USE WITH CAUTION.
+"""
+This module contains the function add that adds two floats,
+and returns their sum.
+"""
 def add(a: float, b: float) -> float:
+    """
+    This function takes two float arguments and returns their sum.
+
+    Args:
+     a (float): First float argument.
+     b (float): Second float argument.
+
+     Returns:
+     float: The sum of the two floats together.
+    """
     return a + b
