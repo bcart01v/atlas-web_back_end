6b8295564564be13e323e328393036a8ab5b5505
Benjamin Carter <38075349+bcart01v@users.noreply.github.com>
2024-08-27 14:23:27 -0500

    Removed Whitespace

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# The Following is a function that takes 2 floats, and adds them together. 
+# The Following is a function that takes 2 floats, and adds them together.
 # USE WITH CAUTION.
 def add(a: float, b: float) -> float:
     return a + b
