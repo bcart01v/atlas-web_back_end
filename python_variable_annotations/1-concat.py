a33d6bbdf057f244a193dbaac2292902ab434e51
Benjamin Carter <38075349+bcart01v@users.noreply.github.com>
2024-08-27 15:38:24 -0500

    Concat!

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+This module contains the function concat that concatenates two strings.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    This function takes two string arguments and concatenates them together.
+
+    Args:
+     a (string): First string argument.
+     b (string): Second string argument.
+
+     Returns:
+     fullstring: Two strings concatenated together.
+    """
+    return str1 + str2
