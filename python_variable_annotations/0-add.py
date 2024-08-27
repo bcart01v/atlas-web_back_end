64738e0cdaa1e0bc09f6e3169bb98927d47f1173
Benjamin Carter <38075349+bcart01v@users.noreply.github.com>
2024-08-27 14:22:22 -0500

    Shortend line of code

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
-# The Following is a function that takes 2 floats, and adds them together. USE WITH CAUTION.
+# The Following is a function that takes 2 floats, and adds them together. 
+# USE WITH CAUTION.
 def add(a: float, b: float) -> float:
     return a + b
