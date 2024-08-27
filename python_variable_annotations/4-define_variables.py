f1b2d9d38880b84cfe560144976787a6b143217f
Benjamin Carter <38075349+bcart01v@users.noreply.github.com>
2024-08-27 16:33:26 -0500

    Defining Variables

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+This script defines variables with specific types annotations.
+"""
+
+# Integer Variable
+a: int = 1
+
+# Float Variable
+pi: float = 3.14
+
+# Boolean Variable
+i_understand_annotations: bool = True
+
+# String Variable.
+school: str = "Holberton"
