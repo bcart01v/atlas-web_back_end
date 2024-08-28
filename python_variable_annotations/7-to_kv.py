faeabd17e1ab57e4b4ddb865a15b9af1cb728c23
Benjamin Carter <38075349+bcart01v@users.noreply.github.com>
2024-08-28 08:41:18 -0500

    Complex Types

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+from typing import Union, Tuple
+"""
+This module takes a mixded list of floats and ints and
+returns the sum of the floats.
+"""
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    This function returns a tuple with the key and the value squared.
+
+    Args:
+        K (str): a string that will be the key.
+        V (Union[int, float]): a number that will be squared.
+
+     Returns:
+        Tuple[str, float]: a tuple with the key and the value squared.
+    """
+    return (k, float(v**2))
