936dd37fb5a5d1512fb2e18ca475d0d422954a75
Benjamin Carter <38075349+bcart01v@users.noreply.github.com>
2024-08-31 12:40:05 -0500

    Async 10 random numbers

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module containing a function that genrates 10 random numbers.
+"""
+import asyncio
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    Collects 10 random numbers from async_generator using an async comprehension.
+
+    Returns:
+        List[float]: A list containing 10 random numbers collected from async_generator.
+    """
+    return [number async for number in async_generator()]
