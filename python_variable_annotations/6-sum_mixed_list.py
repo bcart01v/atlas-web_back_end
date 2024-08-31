abd214fa3061f46f34ba100d822a6e8a88164d41
Benjamin Carter <38075349+bcart01v@users.noreply.github.com>
2024-08-31 13:18:59 -0500

    Documentation Correction

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-from typing import List, Union
 """
 This module takes a mixded list of floats and ints and
 returns the sum of the floats.
 """
+from typing import List, Union
 
 
 def sum_mixed_list(mxd_list: List[Union[int, float]]) -> float:
