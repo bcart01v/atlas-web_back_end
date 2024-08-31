ee6ba66c7e482ee2ff81982411e93a7480a1711f
Benjamin Carter <38075349+bcart01v@users.noreply.github.com>
2024-08-31 13:23:03 -0500

    Pycodestyle corrections

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -6,15 +6,17 @@ import asyncio
 import random
 from typing import AsyncGenerator
 
+
 async def async_generator() -> AsyncGenerator[float, None]:
     """
     Coroutine that asynchronously generates 10 random numbers.
-    
-    It loops 10 times, each time waiting for 1 second, 
+
+    It loops 10 times, each time waiting for 1 second,
     and then yields a random float between 0 and 10.
-    
+
     Returns:
-        AsyncGenerator[float, None]: An asynchronous generator yielding random floats.
+        AsyncGenerator[float, None]: An asynchronous generator
+        yielding random floats.
     """
     for _ in range(10):
         await asyncio.sleep(1)
