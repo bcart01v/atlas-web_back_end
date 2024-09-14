d8e7a8f4ad009c650e7c873b0db4592ede0a346f
Benjamin Carter <38075349+bcart01v@users.noreply.github.com>
2024-09-13 20:30:50 -0500

    Pycodestyle

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -9,13 +9,13 @@ from typing import List
 
 
 def get_db() -> connection.MySQLConnection:
-    """Establishes a connection to the MySQL database using credentials from environment variables"""
+    """Establishes a connection to the MySQL database using
+    credentials from environment variables"""
 
     db_username = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
     db_password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
     db_host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
     db_name = os.getenv('PERSONAL_DATA_DB_NAME')
-
 
     conn = mysql.connector.connect(
         user=db_username,
