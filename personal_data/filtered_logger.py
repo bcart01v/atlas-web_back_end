b328b76e15d58eb94d7e85fd497bd4bb9bb34da3
Benjamin Carter <38075349+bcart01v@users.noreply.github.com>
2024-09-13 20:22:29 -0500

    Forgot to check pycodestyle

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -37,6 +37,7 @@ class RedactingFormatter(logging.Formatter):
         return filter_datum(self.fields, self.REDACTION,
                             original_message, self.SEPARATOR)
 
+
 def get_logger() -> logging.Logger:
     """Creates and returns a logger that handles PII information."""
     logger = logging.getLogger("user_data")
