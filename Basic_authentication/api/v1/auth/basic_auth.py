4e2c62825bd654b12209ec7fe06f348e60a03168
Benjamin Carter <38075349+bcart01v@users.noreply.github.com>
2024-09-19 22:34:10 -0500

    User Credentials

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -37,3 +37,20 @@ class BasicAuth(Auth):
             return decode_base.decode('utf-8')
         except Exception:
             return None
+
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """ extracts user credentials from decode base 64 value"""
+
+        if decoded_base64_authorization_header is None:
+            return None, None
+
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+
+        email, password = decoded_base64_authorization_header.split(':', 1)
+        return email, password
