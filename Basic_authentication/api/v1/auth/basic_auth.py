3746cb68faff54232e57654e003c9b27c5a1e976
Benjamin Carter <38075349+bcart01v@users.noreply.github.com>
2024-09-19 22:41:08 -0500

    User Object

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,9 @@
 """
 from api.v1.auth.auth import Auth
 import base64
+from typing import TypeVar
+
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -53,3 +56,22 @@ class BasicAuth(Auth):
 
         email, password = decoded_base64_authorization_header.split(':', 1)
         return email, password
+
+    def user_object_from_credentials(self, user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """Returns user object based on email and password"""
+
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        user_list = User.search({'email': user_email})
+        if not user_list or len(user_list) == 0:
+            return None
+
+        user = user_list[0]
+        if not user.is_valid_password(user_pwd):
+            return None
+
+        return user
