87dc37b9c11da428ec8dcdb9b807db9a100a5fc3
Benjamin Carter <38075349+bcart01v@users.noreply.github.com>
2024-09-21 15:06:47 -0500

    Updated Response

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -37,7 +37,7 @@ def login():
     session_id = AUTH.create_session(email)
 
     response = make_response
-    (jsonify({"email": email, "message": "logged in"}), 200)
+    (jsonify({"email": email, "message": "logged in"}))
     response.set_cookie('session_id', session_id)
 
     return response
