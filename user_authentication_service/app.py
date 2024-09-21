fa6342ae204645b18d8af71ad0e056a028f5d794
Benjamin Carter <38075349+bcart01v@users.noreply.github.com>
2024-09-21 15:28:56 -0500

    Passord reset route

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -69,6 +69,18 @@ def profile():
     return jsonify({"email": user.email}), 200
 
 
+@app.route('/reset_password', methods=['POST'])
+def get_reset_password_token():
+    email = request.form.get('email')
+
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+    except ValueError:
+        return jsonify({"message": "email not found"}), 403
+
+    return jsonify({"email": email, "reset_token": reset_token}), 200
+
+
 @app.route('/', methods=['GET'])
 def home():
     """ Return a JSON payload. """
