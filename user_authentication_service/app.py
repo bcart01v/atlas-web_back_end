04c3e9e18311d4f33dc47e04256007faab0cc15d
Benjamin Carter <38075349+bcart01v@users.noreply.github.com>
2024-09-21 15:09:28 -0500

    Maybe it doesn't like the line break

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -36,8 +36,7 @@ def login():
 
     session_id = AUTH.create_session(email)
 
-    response = make_response
-    (jsonify({"email": email, "message": "logged in"}))
+    response = make_response(jsonify({"email": email, "message": "logged in"}))
     response.set_cookie('session_id', session_id)
 
     return response
