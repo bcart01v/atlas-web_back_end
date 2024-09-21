9754be4023ce3ef700e6777176bb7ec15f35864f
Benjamin Carter <38075349+bcart01v@users.noreply.github.com>
2024-09-21 00:29:53 -0500

    Creating a session

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -2,9 +2,22 @@
 """ SessionAuth module for api auth
 """
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """ SessionAuth class that inherits from Auth
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ Create session ID for a given USER ID
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid. uuid4())
+
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id
