74c500795ffdb1ab28d0c9dae172e22aa361afac
Benjamin Carter <38075349+bcart01v@users.noreply.github.com>
2024-09-21 13:12:07 -0500

    Finding a user

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -4,6 +4,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.session import Session
 
 from user import Base, User
@@ -41,3 +43,13 @@ class DB:
         self._session.refresh(new_user)
 
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """ Finds a user by arguments"""
+
+        try:
+            return self._session.query(User).filter_by(**kwargs).one()
+        except NoResultFound:
+            raise NoResultFound("No User Found")
+        except InvalidRequestError:
+            raise InvalidRequestError("Invalid Request")
