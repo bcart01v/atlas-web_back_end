7ae9ae44cbc55adf047253dd9b61be1d30505414
Benjamin Carter <38075349+bcart01v@users.noreply.github.com>
2024-09-21 12:07:21 -0500

    Pycodestyle

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -5,6 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class User(Base):
     """ Class for user, to be enhanced later I'm assuming. """
     __tablename__ = 'users'
