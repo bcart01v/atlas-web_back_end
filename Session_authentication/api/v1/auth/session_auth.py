a164945ac4f95100833ea6218381e0cef1bdd8b3
Benjamin Carter <38075349+bcart01v@users.noreply.github.com>
2024-09-21 00:26:26 -0500

    Pycodestyle

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -1,7 +1,8 @@
-#!/usr/bin/env python3 
+#!/usr/bin/env python3
 """ SessionAuth module for api auth
 """
 from api.v1.auth.auth import Auth
+
 
 class SessionAuth(Auth):
     """ SessionAuth class that inherits from Auth
