99f0d6bbe79fb363222529f56d5df9913944be14
Benjamin Carter <38075349+bcart01v@users.noreply.github.com>
2024-09-21 15:06:00 -0500

    Login Function

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ Flask App """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort, make_response
 from auth import Auth
 
 
@@ -23,6 +23,24 @@ def users():
         return jsonify({"email": user.email, "message": "user created"}), 200
     except ValueError:
         return jsonify({"message": "email already exists"}), 400
+
+
+@app.route('/sessions', methods=['POST'])
+def login():
+    """ Login user and create session """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+
+    response = make_response
+    (jsonify({"email": email, "message": "logged in"}), 200)
+    response.set_cookie('session_id', session_id)
+
+    return response
 
 
 @app.route('/', methods=['GET'])
