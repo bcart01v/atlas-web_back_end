0ab8e4bc374b00dd84504b31175a2e3160d0e94c
Benjamin Carter <38075349+bcart01v@users.noreply.github.com>
2024-09-21 00:45:52 -0500

    Returning a user based on session Id

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -30,3 +30,16 @@ class SessionAuth(Auth):
             return None
 
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ returns a user based on session ID"""
+
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return None
+
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return None
+
+        return User.get(user_id)
