3412006565e3f0dbfdb9e08cab218c6360aad64f
Benjamin Carter <38075349+bcart01v@users.noreply.github.com>
2024-09-21 15:44:29 -0500

    Pycodestyle corrections

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -26,23 +26,22 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-            """ Get the page of the dataset.
-            """
-            assert isinstance(page, int) and page > 0, "page should be a positive integer"
-            assert isinstance(page_size, int) and page_size > 0, "page_size should be a positive integer"
+        """ Gets the page"""
+        assert isinstance(page, int) and page > 0, "page should"
+        "be a positive integer"
+        assert isinstance(page_size, int) and page_size > 0, "page_size should"
+        "be a positive integer"
 
-            start_index, end_index = self.index_range(page, page_size)
+        start_index, end_index = self.index_range(page, page_size)
 
-            dataset = self.dataset()
+        dataset = self.dataset()
 
-            if start_index >= len(dataset):
-                return []
-            return dataset[start_index:end_index]
-
-
+        if start_index >= len(dataset):
+            return []
+        return dataset[start_index:end_index]
 
     def index_range(self, page: int, page_size: int) -> tuple:
-        """ Return a tuple of size two containing a start index and an end index
+        """ Return a tuple of size two
         """
 
         start_index = (page - 1) * page_size
