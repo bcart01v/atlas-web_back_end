b62d3679fd20adffbeca680ec98316409fd4b6ad
Benjamin Carter <38075349+bcart01v@users.noreply.github.com>
2024-09-23 14:32:48 -0500

    Parameterizing a unit test using 2 lines

---
+++ Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,18 @@
+#/usr/bin/env python3
+""" Unit Testing"""
+import unittest
+from parameterized import parameterized
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+if __name__ == '__main__':
+    unittest.main()
