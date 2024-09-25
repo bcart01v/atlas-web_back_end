d237876a91b4d8b6a5c11a473cd1f50a984ee727
Benjamin Carter <38075349+bcart01v@users.noreply.github.com>
2024-09-24 21:34:16 -0500

    Corrected nest of liscense test

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -45,16 +45,16 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_public_repos_url.assert_called_once()
             mock_get_json.assert_called_once_with("http://fakeurl.com")
 
-        @parameterized.expand([
-            ({'license': {'key': 'my_license'}}, 'my_license', True),
-            ({'license': {'key': 'other_license'}}, 'other_license', False),
-            ({}, 'NO LICENSE', False),
-        ])
+    @parameterized.expand([
+        ({'license': {'key': 'my_license'}}, 'my_license', True),
+        ({'license': {'key': 'other_license'}}, 'other_license', False),
+        ({}, 'NO LICENSE', False),
+    ])
 
-        def test_has_license(self, repo, license_key, expected):
-            """ Test if it has license"""
-            result = GithubOrgClient.has_license(repo, license_key)
-            self.assertEqual(result, expected)
+    def test_has_license(self, repo, license_key, expected):
+        """ Test if it has license"""
+        result = GithubOrgClient.has_license(repo, license_key)
+        self.assertEqual(result, expected)
 
 
 if __name__ == '__main__':
