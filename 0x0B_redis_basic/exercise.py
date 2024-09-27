31a8e2f9aa999ad8703242838f8606299fc26841
Benjamin Carter <38075349+bcart01v@users.noreply.github.com>
2024-09-27 09:46:48 -0500

    Reading and returning

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -19,3 +19,28 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None
+            ) -> Union[str, bytes, int, float, None]:
+        """
+        Retrieve data from Redis by key. If fn is provided, apply it to
+        the data before returning it.
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Optional[str]:
+        """
+        Retrieve data as a UTF-8 string.
+        """
+        return self.get(key, fn=lambda d: d.decode('utf-8'))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """
+        Retrieve data as an integer.
+        """
+        return self.get(key, fn=int)
