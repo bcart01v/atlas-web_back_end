165a07b7b7f61463641eb080fd6c717f5dd82fc4
Benjamin Carter <38075349+bcart01v@users.noreply.github.com>
2024-10-07 10:52:20 -0500

    Safe Div

---
+++ MySQL_Advanced/10-div.sql
@@ -0,0 +1,12 @@
+-- Function to safely divide two numbers
+
+DELIMITER //
+
+DROP FUNCTION IF EXISTS SafeDiv;
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT DETERMINISTIC
+BEGIN
+	RETURN (IF (b = 0, 0, a / b));
+END //
+
+DELIMITER;
