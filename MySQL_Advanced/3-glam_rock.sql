cc7e0688a74433027ffabe127819a546ce5cadcc
Benjamin Carter <38075349+bcart01v@users.noreply.github.com>
2024-10-07 10:03:04 -0500

    Forgot to rank them

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,5 @@
--- Lists all bands with Glam Rock style.
-
-SELECT band_name, COALESCE(split, 2020) - formed as lifespan FROM metal_bands
-WHERE style LIKE '%Glam rock%' ;
+-- Lists all bands with Glam Rock style
+SELECT band_name, COALESCE(split, 2020) - formed AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
