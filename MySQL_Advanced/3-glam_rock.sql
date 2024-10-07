5e274c6c7f6ba39affe4945140c2ae5854b3a398
Benjamin Carter <38075349+bcart01v@users.noreply.github.com>
2024-10-07 10:54:12 -0500

    Changed select style

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- Lists all bands with Glam Rock style
-SELECT band_name, COALESCE(split, 2020) - formed AS lifespan
+SELECT band_name, TIMESTAMPDIFF(year, MAKEDATE(formed, 1), IF(split IS NULL, CURDATE(), MAKEDATE(split, 1))) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
