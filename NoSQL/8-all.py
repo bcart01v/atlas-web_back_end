3af5be8c61650533a812421e2f24a711fc9e3e26
Benjamin Carter <38075349+bcart01v@users.noreply.github.com>
2024-10-08 21:27:03 -0500

    Returning All

---
+++ NoSQL/8-all.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" list all pymongo
+"""
+
+
+from pymongo import MongoClient
+
+
+def list_all(mongo_collection):
+    """ lists all documents in a collection
+    """
+    return list(mongo_collection.find())
