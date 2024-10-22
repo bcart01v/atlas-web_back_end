1055126d183933d4875df74fa9bc6251c64092be
Benjamin Carter <38075349+bcart01v@users.noreply.github.com>
2024-10-21 22:06:32 -0500

    Same test but Chai assertion

---
+++ unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,42 @@
+import { expect } from 'chai';
+import calculateNumber from './2-calcul_chai.js';
+
+describe('calculateNumber', function() {
+  describe('SUM', function() {
+    it('should return 6 when adding 1.4 and 4.5', function() {
+      expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+    });
+
+    it('should return 5 when adding 1.2 and 3.7', function() {
+      expect(calculateNumber('SUM', 1.2, 3.7)).to.equal(5);
+    });
+  });
+
+  describe('SUBTRACT', function() {
+    it('should return -4 when subtracting 4.5 from 1.4', function() {
+      expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+    });
+
+    it('should return -2 when subtracting 1.6 from -0.4', function() {
+      expect(calculateNumber('SUBTRACT', -1.6, -0.4)).to.equal(-2);
+    });
+  });
+
+  describe('DIVIDE', function() {
+    it('should return 0.2 when dividing 1.4 by 4.5', function() {
+      expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+    });
+
+    it('should return "Error" when dividing 1.4 by 0', function() {
+      expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+    });
+
+    it('should return "Error" when dividing 3.7 by 0', function() {
+      expect(calculateNumber('DIVIDE', 3.7, 0)).to.equal('Error');
+    });
+
+    it('should return 5 when dividing 9.6 by 2.1', function() {
+      expect(calculateNumber('DIVIDE', 9.6, 2.1)).to.equal(5);
+    });
+  });
+});
